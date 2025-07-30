f284034422964234e26d58596ffcd4fbc812ebf8
kvinnxz <keviin9856@gmail.com>
2025-07-30 03:30:23 -0500

    feat: :sparkles: worked in the one part of th eproject
    worked in the one part of th eproject
    
    by @Pico

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import os
 import json
-import controllers.menu_principal as menu
-menu.menu_principal()
-from controllers import (
-    menu_agregar,
-    menu_categorias,
-    menu_buscar,
-    menu_eliminar,
-    menu_editar,
-    menu_guardar,
-    menu_ver
-
-)
+import controllers.menus as men
+if __name__=="__main__":
+    men.menu_principal()
+    
